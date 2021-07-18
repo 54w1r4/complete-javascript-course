07d72623b2369f6a7ca6df0f12938af123ad3beb
Edward Swalker <edwardtang0611@gmail.com>
2021-07-18 18:56:44 +0800

    lesson 12 data types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+let country = 'China';
+let continent = 'Asia';
+let population = 14;
+
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,6 @@
+/*
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 console.log("Edward");
 console.log(17)
@@ -10,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variables name conventions
 let edward_swalker = "Edward Swalker";
 let $function = 27;
 
@@ -20,3 +22,23 @@ let myFirstJob = 'Coder';
 let myCurrentJob = 'Teacher';
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Edward');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+// console.log(year);
+console.log(typeof year);
+
+year = 2021;
+console.log(typeof year);
+
+console.log(typeof null)
