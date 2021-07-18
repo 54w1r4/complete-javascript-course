28eaa9d1fb392d4c31507bda274d20c881126062
Edward Swalker <edwardtang0611@gmail.com>
2021-07-18 17:52:36 +0800

    lesson 10 values and variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Edward");
+console.log(17)
+
+let firstName = "Bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let edward_swalker = "Edward Swalker";
+let $function = 27;
+
+let person = 'edward';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
