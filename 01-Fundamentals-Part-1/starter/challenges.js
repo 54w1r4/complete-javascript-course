4e51ba68d6e4d19c49301d3b51b44f0a3c86e4a2
Edward Swalker <edwardtang0611@gmail.com>
2021-07-26 00:07:19 +0800

    coding challenge 2

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -4,10 +4,13 @@ const heightMark = 1.69;
 const heightJohn = 1.95;
 
 let johnBMI = massJohn / (heightJohn * heightJohn);
-
 let markBMI = massMark / (heightMark * heightMark);
+// let markHigherBMI = markBMI > johnBMI;
 
+// console.log(markHigherBMI, markBMI, johnBMI);
 
-let markHigherBMI = markBMI > johnBMI;
-
-console.log(markHigherBMI);
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+} else {
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`);
+}
