c3ffc3ee1f4a0c9eaaf0755a0a43f088ee394884
Edward Swalker <edwardtang0611@gmail.com>
2021-08-03 01:12:04 +0800

    lecture 22 equality operator '==' & '===' assignments

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,6 @@
-const country = 'China';
+/*const country = 'China';
 const continent = 'Asia';
-let population = 14;
+let population = 14;*/
 
 /*
 console.log(country);
@@ -8,18 +8,18 @@ console.log(continent);
 console.log(population);
 */
 
-const isIsland = true;
-let language;
+/*const isIsland = true;
+let language;*/
 
 /*console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);*/
 
-language = 'Cantonese';
+// language = 'Cantonese';
 // isIsland = false;
 
-console.log(population / 2);
+/*console.log(population / 2);
 population++;
 console.log(population);
 
@@ -28,15 +28,26 @@ console.log(population > finlandPopulation);
 let averagePopulation = 33;
 console.log(averagePopulation < population);
 
-population--;
+population--;*/
 // let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language + '.';
-let description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
-console.log(description);
+/*let description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+console.log(description);*/
 
 // population = 130;
 
+/*
 if (population > 33) {
     console.log(`${country}'s population is above average.`);
 } else {
     console.log(`${country}'s population is ${averagePopulation - population} million below average.`);
+}*/
+
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border`);
+} else {
+    console.log(`No borders`)
 }
