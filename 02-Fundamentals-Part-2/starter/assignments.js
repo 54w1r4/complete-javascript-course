10d41abf8ba993da915b2a715fdb610563181f0d
Edward Swalker <edwardtang0611@gmail.com>
2021-08-12 00:14:25 +0800

    lecture 34 function declaration vs. function expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,11 +1,31 @@
 'use strict';
 
-function describeCountry(country, population, capitalCity) {
+/*function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
-}
+}*/
 
+/*
 const macau = describeCountry('Macau', 0.07, 'Macau');
 const hongKong = describeCountry('Hong Kong', 0.7, 'Hong Kong Island');
 const china = describeCountry('China', 13, 'Beijing');
+*/
+
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
+}
+
+let macau = percentageOfWorld1(0.7);
+let hongKong = percentageOfWorld1(700);
+let china = percentageOfWorld1(1441);
+
+console.log(macau, hongKong, china);
+
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+
+macau = percentageOfWorld2(0.7);
+hongKong = percentageOfWorld2(700);
+china = percentageOfWorld2(1441);
 
 console.log(macau, hongKong, china);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,15 +9,16 @@ if (hasDriversLicense) console.log('I can drive!');*/
 // const interface = 'audio';
 // const private = 23;
 
-function logger() {
+/*function logger() {
     console.log('My name is Elly');
-}
+}*/
 
 //calling / running / invoking function
+/*logger();
 logger();
-logger();
-logger();
+logger();*/
 
+/*
 function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
@@ -27,4 +28,20 @@ const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(appleOrangeJuice);*/
+
+// function declaration
+function calcAge1(birthYear) {
+    return 2021 - birthYear;
+}
+
+const age1 = calcAge1(2004);
+
+// function expression
+const calcAge2 = function (birthYear) {
+    return 2021 - birthYear;
+}
+
+const age2 = calcAge2(2003);
+
+console.log(age1, age2);
